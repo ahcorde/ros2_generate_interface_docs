26462f36b45600c5bebc8a53b8e231f16b1d2fb7
ahcorde <ahcorde@gmail.com>
2020-05-29 10:10:29 +0200

    Added logic to generate msg static HTML docs
    Signed-off-by: ahcorde <ahcorde@gmail.com>

---
+++ ros2_generate_interface_docs/ros2_generate_interface_docs/msg_utils.py
@@ -0,0 +1,72 @@
+# Copyright 2020 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from io import StringIO
+import os
+import time
+
+from ros2_generate_interface_docs import utils
+
+from rosidl_parser.definition import NamespacedType
+
+from rosidl_runtime_py import get_interface_path
+from rosidl_runtime_py import get_message_slot_types
+from rosidl_runtime_py.import_message import import_message_from_namespaced_type
+
+
+message_list_str = """<h2>Message types</h2><div class="msg-list"> <ul> %s </ul> </div>"""
+
+
+def _generate_msg_text_from_spec(package, type_, spec, buff=None, indent=0):
+    if buff is None:
+        buff = StringIO()
+
+    namespaced_type = NamespacedType([package, 'msg'], type_)
+    imported_message = import_message_from_namespaced_type(namespaced_type)
+
+    buff = utils.generate_compact_definition(imported_message,
+                                             buff,
+                                             indent,
+                                             get_message_slot_types,
+                                             False)
+    return buff.getvalue()
+
+
+def generate_msg_doc(msg, msg_template, path):
+    package, interface, base_type = utils.resource_name(msg)
+    d = {'name': msg, 'ext': 'msg', 'type': 'Message',
+         'package': package, 'base_type': base_type,
+         'date': str(time.strftime('%a, %d %b %Y %H:%M:%S'))}
+    file_path = get_interface_path(msg)
+    with open(file_path, 'r', encoding='utf-8') as h:
+        spec = h.read().rstrip()
+
+    d['compact_definition'] = _generate_msg_text_from_spec(package, base_type, spec)
+    d['raw_text'] = utils.generate_raw_text(spec)
+    return msg_template % d
+
+
+def generate_msg_index(package, file_directory, msgs):
+    package_message_dic = utils._TEMPLATE_DIC
+    package_message_dic['package'] = package
+    package_message_dic['date'] = str(time.strftime('%a, %d %b %Y %H:%M:%S'))
+    if msgs:
+        package_message_dic['msg_list'] = message_list_str % '\n'.join(
+            [' <li>%s</li>' % utils._href('../../html/' + package + '/' + msg + '.html', msg)
+             for msg in msgs])
+    msg_index_template = utils.load_template('msg-index.template')
+    file_p = os.path.join(file_directory, 'index-msg.html')
+    text = msg_index_template % package_message_dic
+    with open(file_p, 'w') as f:
+        f.write(text)
---
+++ ros2_generate_interface_docs/ros2_generate_interface_docs/utils.py
@@ -0,0 +1,123 @@
+# Copyright 2020 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import sys
+
+from rosidl_parser.definition import AbstractNestedType, BASIC_TYPES, NamespacedType
+
+_TEMPLATES_DIR = 'templates'
+_TEMPLATE_DIC = {'package': '',
+                 'msg_list': '',
+                 'srv_list': '',
+                 'action_list': '',
+                 'date': ''}
+IGNORED_KEYS = ['__slots__', '__doc__', '_fields_and_field_types', 'SLOT_TYPES', '__init__',
+                '__repr__', '__eq__', '__hash__', 'get_fields_and_field_types', '__module__']
+
+
+def _href(link, text):
+    return '<a href="%s">%s</a>' % (link, text)
+
+
+def generate_raw_text(raw_text):
+    raw_test_str = ''
+    for line in raw_text.split(os.linesep):
+        line = line.replace(' ', '&nbsp;')
+        parts = line.split('#')
+        if len(parts) > 1:
+            raw_test_str = raw_test_str + parts[0] \
+                + '<font color="blue">#%s</font><br/>' % ('#'.join(parts[1:]))
+        else:
+            raw_test_str = raw_test_str + '%s<br/>' % parts[0]
+    return raw_test_str
+
+
+def resource_name(resource):
+    """Return the resource name."""
+    if '/' not in resource:
+        return '', '', resource
+    values = resource.split('/')
+    if len(values) != 3:
+        raise ValueError(...)
+    return tuple(values)
+
+
+def get_templates_dir():
+    """Return template directory."""
+    return os.path.join(os.path.dirname(__file__), _TEMPLATES_DIR)
+
+
+def copy_css_style(folder_name):
+    """Copy the css style file in the folder_name."""
+    style_css = load_template('styles.css')
+    with open(os.path.join(folder_name, 'styles.css'), 'w') as f:
+        f.write(style_css)
+
+    msg_style_css = load_template('msg-styles.css')
+    with open(os.path.join(folder_name, 'msg-styles.css'), 'w') as f:
+        f.write(msg_style_css)
+
+
+def load_template(filename):
+    """
+    Look up file within rosdoc ROS package.
+
+    return its content, may sys.exit on error.
+    Contents are cached with filename as key.
+
+    :returns: cached file contents
+    """
+    filename = os.path.join(get_templates_dir(), filename)
+    if not os.path.isfile(filename):
+        sys.stderr.write("Cannot locate template file '%s'\n" % (filename))
+        sys.exit(1)
+    with open(filename, 'r') as f:
+        content = f.read()
+        if not content:
+            raise Exception("Template file '%s' is empty\n" % (filename))
+        return content
+
+
+def generate_compact_definition(imported_interface, buff, indent, get_slot_types, need_separator):
+    ignored_keys_ = IGNORED_KEYS
+    ignored_keys_ += list(imported_interface.get_fields_and_field_types().keys())
+    ignored_keys_ += ['_'+x for x in imported_interface.get_fields_and_field_types().keys()]
+    for key in imported_interface.__dict__.keys():
+        if key not in ignored_keys_:
+            buff.write('%s %s=%s<br/>' % ('&nbsp;' * indent,
+                                          key,
+                                          getattr(imported_interface, key)))
+#
+    fields = imported_interface.get_fields_and_field_types()
+    for field in fields.keys():
+        if(fields[field] not in BASIC_TYPES and
+           'string' not in fields[field]):
+            slot_type = get_slot_types(imported_interface)[field]
+            if(isinstance(slot_type, AbstractNestedType)):
+                if(isinstance(slot_type.value_type, NamespacedType)):
+                    pkg_name, in_type, msg_name = map(str,
+                                                      slot_type.value_type.namespaced_name())
+                    href_str = _href('../../' + pkg_name + '/msg/' + msg_name + '.html',
+                                     '/'.join(slot_type.value_type.namespaced_name()))
+                    buff.write('%s%s %s<br />' % ('&nbsp;' * indent, href_str, field))
+            else:
+                pkg_name, msg_name = fields[field].split('/')
+                href_str = _href('../../' + pkg_name + '/msg/' + msg_name + '.html', fields[field])
+                buff.write('%s%s %s<br />' % ('&nbsp;' * indent, href_str, msg_name))
+        else:
+            buff.write('%s%s %s<br />' % ('&nbsp;' * indent, fields[field], field))
+        if(need_separator):
+            buff.write('<hr/>')
+    return buff
